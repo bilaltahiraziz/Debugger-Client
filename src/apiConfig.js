fefbea85b11260a350d3b6f616b9a6e6a6ad560f
bilaltahiraziz <bilal.aziz@live.com>
2022-05-26 15:20:18 -0500

    made changes to url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://pure-cove-81133.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
